27edf68d45de171b47271e3be087883fb2020c4d
Carmen García Alonso <carmengaq24teis@gmail.com>
2026-03-05 09:41:13 +0100

    ajustando hello-kitty en pagina not found

--- frontend/src/components/NotFound.vue
+++ frontend/src/components/NotFound.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="row">
+    <div class="row justify-content-center align-items-center text-center mt-5">
         <h1>404</h1>
-        <img src="../assets/HelloKitty.png" alt="error_hello_kitty">
+        <img class="w-50" src="../assets/HelloKitty.png" alt="error_hello_kitty">
         <br></br>
         <h2>Página No encontrada</h2>
         <p>Perdona, nos hemos equivocado</p>
